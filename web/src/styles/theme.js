b39d33777c33ec45de1584d2cc43a66728b5bdd5
cVitorAndrade <vitorandrade1804@gmail.com>
2024-01-25 21:42:52 -0300

    feat: create application theme and global style

---
+++ web/src/styles/theme.js
@@ -0,0 +1,18 @@
+export default {
+    COLORS: {
+        WHITE_900: "#FFFFFF",
+        WHITE_800: "#F2F2F2",
+
+        GRAY_900: "#E0E0E0",
+        GRAY_800: "#828282",
+        GRAY_700: "#BDBDBD",
+
+        BLACK_900: "#120F13",
+        BLACK_800: "#0B090C",
+        BLACK_700: "#252329",
+
+        BLUE_900: "#2F80ED",
+
+        RED_900: "#EB5757",
+    }
+}
