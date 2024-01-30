b9fdb80120ba957d9b5ced82a210c2dc77d84dfd
cVitorAndrade <vitorandrade1804@gmail.com>
2024-01-29 22:31:15 -0300

    feat: add error handling

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,10 @@
+require("express-async-errors");
+
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
+
+const AppError = require("./utils/AppError");
 
 const { initializeSocket } = require("./socket");
 
@@ -16,6 +20,21 @@ const routes = require("./routes");
 app.use(express.json());
 
 app.use(routes);
+
+app.use(( error, request, response, next) => {
+    if ( error instanceof AppError ) {
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        });
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error."
+    })
+});
+
 
 const Port = 3333;
 
